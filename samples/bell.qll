b703450f8e9df46d20b1ac79bb36d3ef444b3a9e
Salvador de la Puente González <salva@unoyunodiez.com>
2019-08-05 12:53:09 +0200

    Simplifying quoll syntax

--- samples/bell.qll
+++ samples/bell.qll
@@ -2,24 +2,30 @@ from quoll.preamble import *
 from quoll.assertions import *
 
 
-def bell_state(c: Qubit, t: Qubit) -> Unit[Adj]:
+@qasm
+def bell_state(c: Qubit, t: Qubit):
   H(c)
   Controlled[X]([c], t)
 
 
 def test_bell_state():
-  with reset(allocate(1, 1)) as (c, t):
+  with allocate(1, 1) as (c, t):
     bell_state(c, t)
-    result = measure(t)
     assertProb(
       [measure(t), measure(c)], [True, True],
-      prob=0.5, msg='11 combination should be 50%.'
+      prob=0.5, msg='11 combination should be 50%.',
+      delta=1E-1
     )
     assertProb(
       [measure(t), measure(c)], [False, False],
-      prob=0.5, msg='00 combination should be 50%.'
+      prob=0.5, msg='00 combination should be 50%.',
+      delta=1E-1
     )
     print('Everything is OK! Done.')
 
-if __name__ == '__main__':
+
+def main():
   test_bell_state()
+
+if __name__ == '__main__':
+  main()
--- samples/bell_translated.py
+++ samples/bell_translated.py
@@ -1,55 +1,41 @@
-from quoll.preamble import *
-from quoll.assertions import *
-
 # The boilerplate module includes the repetitive extra code needed for
 # quoll code.
 import quoll.boilerplate as bp
 
-# Unit are translated to singleton callables.
-@bp.singleton
-class bell_state:
+from quoll.preamble import *
+from quoll.assertions import *
 
-  def __call__(self, c, t):
-    H(c)
-    Controlled[X]([c], t)
+# The decorator @qasm mark the implementation of a unitary.
+# No parameters means nothing to autogenerate.
+@qasm
+def bell_state(c: Qubit, t: Qubit):
+  H(c)
+  Controlled[X]([c], t)
 
-  # Since this Unit is also Adj, __adj__ is automatically added.
-  # The implementation is generated automatically from the definition.
-  @property
-  def __adj__(self):
-    def _adjoint(c, t):
-      Adjoint[Controlled[X]]([c], t)
-      Adjoint[H](c)
-
-    _adjoint.__adj__ = self
-    return _adjoint
-
-  @property
-  def __ctl__(self):
-    raise NotImplementedError
 
 # Not a unit, so not translated, so far:
 def test_bell_state():
-  with reset(allocate(1, 1)) as (c, t):
+  with allocate(1, 1) as (c, t):
     bell_state(c, t)
     # The static analisys conclude this is a valid program since:
     # 1. There is no further circuit alterations after measurements.
     # 2. There is no dynamic behaviour.
     #
     # So, all measurements get group and sorted.
-    _mp_t = measure(t)
-    _mp_c = measure(c)
+    _mp1 = measure(t)
+    _mp2 = measure(c)
+    _mp3 = measure(t)
+    _mp4 = measure(c)
     # And the circuit is executed. Arguments sorted by lexicographical order.
-    _m_c, _m_t = bp.execute(_mp_c, _mp_t)
+    _m1, _m2, _m3, _m4 = bp.execute(_mp1, _mp2, _mp3, _mp4)
 
-    result = _m_t
     assertProb(
-      [_m_t, _m_c], [True, True],
+      [_m1, _m2], [True, True],
       prob=0.5, msg='11 combination should be 50%.',
       delta=1E-1
     )
     assertProb(
-      [_m_t, _m_c], [False, False],
+      [_m3, _m4], [False, False],
       prob=0.5, msg='00 combination should be 50%.',
       delta=1E-1
     )
