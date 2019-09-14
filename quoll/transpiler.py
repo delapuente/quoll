604dfaf3d170b37f8448be9a32334c0fcca90147
Salvador de la Puente González <salva@unoyunodiez.com>
2019-09-14 18:56:21 +0200

    Implement the controlled functor generator

--- quoll/transpiler.py
+++ quoll/transpiler.py
@@ -1,6 +1,6 @@
 from typing import Any, List
 import ast
-from ast import AST, dump, NodeTransformer, copy_location, Call, Name, Load, Expr, With, FunctionDef, NameConstant, Index, Subscript
+from ast import AST, dump, NodeTransformer, copy_location, Call, Name, Load, With, FunctionDef, NameConstant, Index, Subscript, arg, Expression
 from functools import partial
 from dataclasses import dataclass, field
 
@@ -30,6 +30,34 @@ class Translator(NodeTransformer):
     self._context = context
 
 
+class ControlledComputer(Translator):
+
+  def __init__(self, *args, control_param_name, **kwargs):
+    super().__init__(*args, **kwargs)
+    self._control_param_name = control_param_name
+
+  def visit_Call(self, node: Call):
+    if _is_operation_call(node, self._context.operation_table):
+      node.func = copy_location(_wrap_in_controlled(node.func), node)
+      node.args.insert(0, copy_location(
+        Name(self._control_param_name, ctx=Load()), node.args[0]))
+      return node
+
+    if _is_variant_call(node, self._context.operation_table):
+      functor_application = node.func
+      if _identify_signature(functor_application) == 'Id':
+        node.func = copy_location(_wrap_in_controlled(node.func), node)
+        node.args.insert(0, copy_location(
+          Name(self._control_param_name, ctx=Load()), node.args[0]))
+
+      else:
+        node.args[0] = copy_location(_extend_control_data(
+          node.args[0], self._control_param_name), node.args[0])
+
+    self.generic_visit(node)
+    return node
+
+
 class AdjointComputer(Translator):
 
   def visit_Call(self, node):
@@ -41,7 +69,7 @@ class AdjointComputer(Translator):
     return node
 
   def visit_Subscript(self, node):
-    if _is_operation_functor(node, self._context.operation_table):
+    if _is_functor_application(node, self._context.operation_table):
       return copy_location(_wrap_in_adjoint(node), node)
 
     self.generic_visit(node)
@@ -49,22 +77,46 @@ class AdjointComputer(Translator):
 
 
 def _wrap_in_adjoint(node):
-  adjoint_wrapper = ast.parse('Adjoint[_]', mode='single').body[0].value
-  adjoint_wrapper.slice.value = node
-  return adjoint_wrapper
+  return _wrap_in_functor(node, 'Adjoint')
+
+def _wrap_in_controlled(node):
+  return _wrap_in_functor(node, 'Controlled')
+
+def _wrap_in_functor(node, name):
+  wrapper = ast.parse(f'{name}[_]', mode='single').body[0].value
+  wrapper.slice.value = node
+  return wrapper
+
+def _extend_control_data(original_control_data_node, extension_name):
+  extended = ast.parse(f'(_) + {extension_name}').body[0].value
+  extended.left = original_control_data_node
+  return extended
+
+def _is_operation_call(call, symbol_table):
+  return isinstance(call.func, Name) and call.func.id in symbol_table
 
 
-def _is_operation_call(node, symbol_table):
-  return isinstance(node.func, Name)\
-    and node.func.id in symbol_table
+# TODO: A variant is the result of applying a functor to an operation.
+def _is_variant_call(call, symbol_table):
+  return isinstance(call.func, Subscript)\
+    and _is_functor_application(call.func, symbol_table)
 
+def _is_functor_application(subscript, symbol_table):
+  return isinstance(subscript.value, Name)\
+    and subscript.value.id in ['Adjoint', 'Controlled']\
+    and isinstance(subscript.slice, Index)\
+    and isinstance(subscript.slice.value, Name)\
+    and subscript.slice.value.id in symbol_table\
 
-def _is_operation_functor(node, symbol_table):
-  return isinstance(node.value, Name)\
-    and node.value.id in ['Adjoint', 'Controlled']\
-    and isinstance(node.slice, Index)\
-    and isinstance(node.slice.value, Name)\
-    and node.slice.value.id in symbol_table
+def _identify_signature(functor_application):
+  if not isinstance(functor_application, Subscript):
+    return 'Id'
+
+  if isinstance(functor_application.value, Name)\
+    and functor_application.value.id == 'Controlled':
+    return 'Controlled'
+
+  return _identify_signature(functor_application.slice)
 
 
 class BodyTranslator(Translator):
@@ -130,6 +182,13 @@ class BodyTranslator(Translator):
           map(fix_location, _wire_adjoints(node, adjoint_implementation))
         )
 
+      if _auto_controlled(node):
+        controlled_implementation = fix_location(self._compute_controlled(node))
+        new_nodes.append(controlled_implementation)
+        new_nodes.extend(
+          map(fix_location, _wire_controlled(node, controlled_implementation))
+        )
+
       return new_nodes
 
     self.generic_visit(node)
@@ -142,6 +201,20 @@ class BodyTranslator(Translator):
     adjoint.decorator_list = []
     AdjointComputer(self._context).visit(adjoint)
     return adjoint
+
+  def _compute_controlled(self, node: FunctionDef):
+    import copy
+    adjoint = copy.deepcopy(node)
+    adjoint.name = f'_{node.name}_ctl'
+    # TODO: Calculate a unique name for the control parameter
+    control_param_name = '__control'
+    adjoint.args.args.insert(
+      0, copy_location(arg(control_param_name, annotation=None), node))
+    adjoint.decorator_list = []
+    ControlledComputer(
+      self._context, control_param_name=control_param_name).visit(adjoint)
+    return adjoint
+
 
   def _assign_proxy(self, m_node: Call, proxy_name: str, node: Call):
     return copy_location(_assign_to_proxy(m_node, proxy_name), node)
@@ -167,13 +240,27 @@ def _wire_adjoints(node: FunctionDef, adjoint_node: FunctionDef):
     ast.parse(f'setattr({adjoint_node_name}, \'__adj__\', {node_name})').body[0]
   ]
 
+def _wire_controlled(node: FunctionDef, controlled_node: FunctionDef):
+  node_name = node.name
+  controlled_node_name = controlled_node.name
+  return [
+    ast.parse(f'setattr({node_name}, \'__ctl__\', {controlled_node_name})').body[0],
+    ast.parse(f'setattr({controlled_node_name}, \'__ctl__\', {controlled_node_name})').body[0]
+  ]
+
+
 
 def _auto_adjoint(node: FunctionDef):
+  return _some_kw_match('adj', True, node.decorator_list[-1].keywords)
+
+def _auto_controlled(node: FunctionDef):
+  return _some_kw_match('ctl', True, node.decorator_list[-1].keywords)
+
+def _some_kw_match(name, value, kwargs):
   def id_is_adj(kw):
-    return kw.arg == 'adj' and isinstance(kw.value, NameConstant) and kw.value.value is True
+    return kw.arg == name and isinstance(kw.value, NameConstant) and kw.value.value == value
 
-  return any(map(id_is_adj, node.decorator_list[-1].keywords))
-
+  return any(map(id_is_adj, kwargs))
 
 def _is_measurement(node: Call):
   return isinstance(node.func, Name) and node.func.id == 'measure'
