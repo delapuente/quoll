c6bb79284ecec631f234f75fdc1e0ce69f1b0d1e
Salvador de la Puente González <salva@unoyunodiez.com>
2019-09-20 18:07:19 +0200

    Allow interpretation of the measurements into unsigned ints and bool types

--- quoll/measurements.py
+++ quoll/measurements.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import Tuple
 
 from qiskit import ClassicalRegister, QuantumCircuit, QuantumRegister
 from qiskit.result import Result
@@ -18,3 +19,20 @@ class Measurement:
   proxy: MeasurementProxy
 
   result: Result
+
+  measurement_bitstring: int = field(init=False)
+
+  def __post_init__(self):
+    whole_memory_bitstring, _ = max(
+      self.result.get_counts().items(), key=lambda item: item[1])
+
+    # TODO: Endian mess. Should clarify this is this way and add some tests.
+    registers_bitstrings = list(reversed(whole_memory_bitstring.split(' ')))
+    self.measurement_bitstring = registers_bitstrings[
+      self.proxy.circuit.cregs.index(self.proxy.classical_register)]
+
+  def __int__(self):
+    return int(self.measurement_bitstring, 2)
+
+  def __bool__(self):
+    return bool(int(self))
