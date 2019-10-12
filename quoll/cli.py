88c60f7970c7bd635d0686d1ddc80391734927f7
Salvador de la Puente González <salva@unoyunodiez.com>
2019-10-13 00:14:36 +0200

    Add configuration file option

--- quoll/cli.py
+++ quoll/cli.py
@@ -4,7 +4,7 @@ Provide command line interface. Basic usage, considering a Qiskit account:
   quoll -b ibmq:ibmqx4 samples.bell:main
 
 """
-import sys
+import os
 from argparse import ArgumentParser
 from importlib import import_module
 
@@ -12,7 +12,8 @@ from importlib import import_module
 def build_parser():
   parser = ArgumentParser()
   parser.add_argument('modulefunc', type=str, help='function path in the format \'package.module:function_name\'.')
-  parser.add_argument('-b', '--backend', type=str, help='backend name the format \'provider:backend_name\'')
+  parser.add_argument('-b', '--backend', type=str, help='backend name the format \'provider:backend_name\'.')
+  parser.add_argument('-c', '--config', type=str, default='quoll.ini', help='Quoll configuration with the exection options among others.')
   parser.add_argument('--show-python', action='store_true', help='generates *.qll.py files with the Python transpiled version of the source.')
   return parser
 
@@ -23,10 +24,12 @@ def main():
   import quoll.config
   quoll.config.set_show_python(args.show_python)
 
+  if args.config:
+    filepath = os.path.join(os.getcwd(), args.config)
+    quoll.config.set_config_file(filepath)
+
   if args.backend:
-    provider, backend_name = args.backend.split(':')
-    backend = quoll.config.resolve_backend(provider, backend_name)
-    quoll.config.set_backend(backend)
+    quoll.config.set_backend(args.backend)
 
   module_and_function = args.modulefunc.split(':')
   if len(module_and_function) == 2:
--- quoll/config.py
+++ quoll/config.py
@@ -1,4 +1,6 @@
 import os
+from configparser import ConfigParser
+
 import qiskit
 import quoll.boilerplate as bp
 from quoll.unparser import unparse
@@ -11,18 +13,27 @@ if hasattr(qiskit, 'IBMQ'):
 if hasattr(qiskit, 'Aer'):
   _PROVIDERS['aer'] = qiskit.Aer
 
-_BACKEND = qiskit.BasicAer.get_backend('qasm_simulator')
+_CONFIG_FILE = 'quoll.ini'
+
+_BACKEND = 'basicaer:qasm_simulator'
 
 _SHOW_PYTHON = False
 
+def get_config_file():
+  return _CONFIG_FILE
+
+def set_config_file(config_file):
+  global _CONFIG_FILE
+  _CONFIG_FILE = config_file
+
 def get_backend():
-  return _BACKEND
+  return _resolve_backend(*_BACKEND.split(':'))
 
 def set_backend(backend):
   global _BACKEND
   _BACKEND = backend
 
-def resolve_backend(provider_name, backend_name):
+def _resolve_backend(provider_name, backend_name):
   if provider_name not in _PROVIDERS:
     raise ValueError(f'No provider with id {provider_name}')
 
@@ -53,3 +64,19 @@ def _resolve_path(current, relative):
     return relative
 
   return os.path.normpath(os.path.join(current, relative))
+
+def get_shots():
+  fallback = 1024
+  config = ConfigParser()
+  config.read(get_config_file())
+  section = _get_backend_matching_section(_BACKEND, config)
+  return config[section].getint(
+    'shots', fallback=fallback) if section else fallback
+
+def _get_backend_matching_section(backend_id, config):
+  sections = config.keys()
+  for one_section in sections:
+    if one_section.startswith('backend:'):
+      _, config_backend_id = one_section.split(':', 1)
+      if config_backend_id == '*' or config_backend_id == backend_id:
+        return one_section
